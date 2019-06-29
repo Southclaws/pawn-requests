5d90afb22c6abcb5cc3e15210ea3192f81764003
SyS <sreyassreelal@gmail.com>
2019-06-29 06:12:00 +0000

    pascal case methods

--- src/method.rs
+++ src/method.rs
@@ -1,30 +1,30 @@
 enum_from_primitive! {
 #[derive(Debug, PartialEq, Clone)]
 pub enum Method {
-    HTTP_METHOD_GET,
-    HTTP_METHOD_HEAD,
-    HTTP_METHOD_POST,
-    HTTP_METHOD_PUT,
-    HTTP_METHOD_DELETE,
-    HTTP_METHOD_CONNECT,
-    HTTP_METHOD_OPTIONS,
-    HTTP_METHOD_TRACE,
-    HTTP_METHOD_PATCH,
+    HttpMethodGet,
+    HttpMethodHead,
+    HttpMethodPost,
+    HttpMethodPut,
+    HttpMethodDelete,
+    HttpMethodConnect,
+    HttpMethodOptions,
+    HttpMethodTrace,
+    HttpMethodPatch,
 }
 }
 
 impl Method {
     pub fn into(self) -> reqwest::Method {
         match self {
-            Method::HTTP_METHOD_GET => reqwest::Method::GET,
-            Method::HTTP_METHOD_HEAD => reqwest::Method::HEAD,
-            Method::HTTP_METHOD_POST => reqwest::Method::POST,
-            Method::HTTP_METHOD_PUT => reqwest::Method::PUT,
-            Method::HTTP_METHOD_DELETE => reqwest::Method::DELETE,
-            Method::HTTP_METHOD_CONNECT => reqwest::Method::CONNECT,
-            Method::HTTP_METHOD_OPTIONS => reqwest::Method::OPTIONS,
-            Method::HTTP_METHOD_TRACE => reqwest::Method::TRACE,
-            Method::HTTP_METHOD_PATCH => reqwest::Method::PATCH,
+            Method::HttpMethodGet => reqwest::Method::GET,
+            Method::HttpMethodHead => reqwest::Method::HEAD,
+            Method::HttpMethodPost => reqwest::Method::POST,
+            Method::HttpMethodPut => reqwest::Method::PUT,
+            Method::HttpMethodDelete => reqwest::Method::DELETE,
+            Method::HttpMethodConnect => reqwest::Method::CONNECT,
+            Method::HttpMethodOptions => reqwest::Method::OPTIONS,
+            Method::HttpMethodTrace => reqwest::Method::TRACE,
+            Method::HttpMethodPatch => reqwest::Method::PATCH,
         }
     }
 }
